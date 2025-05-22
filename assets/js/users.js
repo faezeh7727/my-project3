f18abc495f13d4b8b942bdcca16f07d3cf70aa10
faezeh7727 <gfaeze561@gmail.com>
2025-05-21 22:04:00 -0700

    comit neww

--- assets/js/users.js
+++ assets/js/users.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems,{});
   });
+
+  const adduserbutton = document.getElementById('add-user-button')
+  adduserbutton.addEventListener('click' , ()=>{
+    document.querySelector('.add-user-modal').classlist.add('show')
+  })
